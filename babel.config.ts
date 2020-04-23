07c28c93dd9fbfe491d5826a9aabf8b6e56dfaaf
shinvey <shinvey@qq.com>
2020-04-23 23:32:23 +0800

    TypeScript + babel

---
+++ babel.config.ts
@@ -0,0 +1,30 @@
+import { ConfigFunction } from '@babel/core'
+
+const configFn: ConfigFunction = api => {
+  api.cache.forever()
+
+  return {
+    presets: [
+      '@babel/preset-env',
+      '@babel/preset-typescript',
+    ],
+    env: {
+      build: {
+        /**
+         * 当BABEL_ENV=build，将ignore配置合并到当前父级ignore配置中
+         */
+        ignore: [
+          '**/*.test.tsx',
+          '**/*.test.ts',
+          '**/*.story.tsx',
+          '__snapshots__',
+          '__tests__',
+          '__stories__',
+        ],
+      },
+    },
+    ignore: ['node_modules'],
+  }
+}
+
+export default configFn
